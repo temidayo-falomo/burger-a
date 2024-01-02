d05344bd2b8ae30fd7b1aeefc7ff4eee2f723943
temidayo <temmideee@gmail.com>
2024-01-02 23:09:43 +0100

    updates

--- src/pages/makeBurger/MakeBurger.jsx
+++ src/pages/makeBurger/MakeBurger.jsx
@@ -7,14 +7,31 @@ import Navbar from "../../components/navbar/Navbar";
 import Footer from "../../components/footer/Footer";
 
 function MakeBurger() {
+  //
   const [getCutletCount, setGetCutletCount] = useState(0);
+
+  //
   const [getMayoCount, setGetMayoCount] = useState(0);
+
+  //
   const [getOnionCount, setGetOnionCount] = useState(0);
+
+  //
   const [getTomatoCount, setGetTomatoCount] = useState(0);
+
+  //
   const [getCucumberCount, setGetCucumberCount] = useState(0);
+
+  //
   const [getCheeseCount, setGetCheeseCount] = useState(0);
+
+  //
   const [getSaladCount, setGetSaladCount] = useState(0);
+
+  //
   const [getBunCount, setGetBunCount] = useState(0);
+
+  //
   const [showModal, setShowModal] = useState(false);
 
   return (
